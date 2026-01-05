532b1999becd9ea3a66dbe7b7474d0f8fee473a0
Zsolt Tovis <tovis.zsolt@gmail.com>
2026-01-05 08:21:48 +0100

    Refactor type definitions to enhance className handling in JSX elements

--- src/clsx-react.d.ts
+++ src/clsx-react.d.ts
@@ -1,4 +1,4 @@
-import 'react';
+import * as React from 'react';
 
 type ClassNameValue =
   | string
@@ -9,8 +9,16 @@ type ClassNameValue =
   | Record<string, boolean | null | undefined>
   | ClassNameValue[];
 
+type WithCustomClassName<P> = Omit<P, 'className'> & {
+  className?: ClassNameValue;
+};
+
 declare global {
   namespace JSX {
+    type IntrinsicElements = {
+      [K in keyof React.JSX.IntrinsicElements]: WithCustomClassName<React.JSX.IntrinsicElements[K]>;
+    };
+
     interface IntrinsicAttributes {
       className?: ClassNameValue;
     }
